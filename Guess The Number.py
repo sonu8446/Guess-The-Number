8a8c80dd9f45cd7687f577d29a2196e7b12c1412
sonu8446 <66254694+sonu8446@users.noreply.github.com>
2020-07-31 13:36:04 +0530

    Update Guess The Number.py

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,14 +1,20 @@
 import random 
 class guessnumber:
-    def check(self,uv,rv):
-        if rv=uv:
+    def __init__(self,rv,uv):
+        self.uv=uv
+        self.rv=rv
+    def check(self):
+        if self.rv==self.uv:
             print("Grate! Your guess is correct")
         else:
             print("Oo so sad! Your guess is wrong")
-gn=guessnumber()
+            print("You should have entered",rv)
+
+
 rv=random.randint(0,20)#random value
 uv=int(input("enter any number between 0 to 20")) #user value
+gn=guessnumber(rv,uv)
 if uv<20:
-    gn.check(rv,uv)
+    gn.check()
 else:
     print("invalid input")
